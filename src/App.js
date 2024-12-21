5bdc89a2097ab83f52989a1c154b84c11a2c0b9a
waghraj1699 <waghrajpatil010699@gmail.com>
2024-12-21 11:12:34 +0000

    layoutBuilder

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import {Component} from 'react'
+import ConfigurationController from './components/ConfigurationController'
+import Layout from './components/Layout'
+import ConfigurationContext from './context/ConfigurationContext'
+import './App.css'
+
+class App extends Component {
+  state = {
+    showContent: true,
+    showLeftNavbar: true,
+    showRightNavbar: true,
+  }
+
+  onChangeShowContent = () => {
+    this.setState(prevState => ({showContent: !prevState.showContent}))
+  }
+
+  onChangeShowLeftNavbar = () => {
+    this.setState(prevState => ({showLeftNavbar: !prevState.showLeftNavbar}))
+  }
+
+  onChangeShowRightNavbar = () => {
+    this.setState(prevState => ({showRightNavbar: !prevState.showRightNavbar}))
+  }
+
+  render() {
+    const {showContent, showLeftNavbar, showRightNavbar} = this.state
+    return (
+      <ConfigurationContext.Provider
+        value={{
+          showContent,
+          showLeftNavbar,
+          showRightNavbar,
+          onToggleShowContent: this.onChangeShowContent,
+          onToggleShowLeftNavbar: this.onChangeShowLeftNavbar,
+          onToggleShowRightNavbar: this.onChangeShowRightNavbar,
+        }}
+      >
+        <div>
+          <ConfigurationController />
+          <Layout />
+        </div>
+      </ConfigurationContext.Provider>
+    )
+  }
+}
+export default App
